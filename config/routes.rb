47661ddcfbc48a28234b63316f4eaf059775c535
kaelonswanton <kaelonswanton@gmail.com>
2023-11-12 04:30:36 -0800

    UI changes and User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#about"
+
 end
